1d8cd1b2ec2db3d1229e9b52f6f2135abd1eef16
Sai Sasidhar Maddali <mail@sasidhar.in>
2021-08-25 19:14:12 +0200

    Add ctags tooling for vim

---
+++ vim/gen_file_list_for_gutentags.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+#
+# Slight modified version based on https://gist.github.com/tim-connolly/2db6d7fcdd7dc1cc589f66e06532de24
+#
+
+import os
+import sys
+
+
+def get_base_prefix_compat():
+    """Get base/real prefix, or sys.prefix if there is none."""
+    return (
+        getattr(sys, "base_prefix", None)
+        or getattr(sys, "real_prefix", None)
+        or sys.prefix
+    )
+
+
+def in_virtualenv():
+    return get_base_prefix_compat() != sys.prefix
+
+
+def files_in_tree(dir):
+    subfolders, files = [], []
+
+    for f in os.scandir(dir):
+        if f.is_dir():
+            subfolders.append(f.path)
+        if f.is_file():
+            files.append(f.path)
+
+    for d in subfolders:
+        f = files_in_tree(d)
+        files.extend(f)
+    return files
+
+
+def print_all_files(dirs):
+    files = []
+    for d in dirs:
+        if not os.path.isdir(d):
+            continue
+        dirpath = os.path.abspath(os.path.expanduser(d))
+        f = files_in_tree(dirpath)
+        files.extend(f)
+
+    print("\n".join("{}".format(f) for f in files if f.endswith(".py")))
+
+
+if in_virtualenv():
+    print_all_files(sys.path)
+else:
+    print_all_files(".")
